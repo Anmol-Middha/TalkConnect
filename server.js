3f9af2c2b64d80891c1223cc42081c713b4bbf32
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-18 19:12:22 +0530

    update

--- server.js
+++ server.js
@@ -9,5 +9,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
 
+app.get('/*', function(req, res){
+    console.log("hello world");
+    res.send("hello");
+})
+
 app.use('/google-authfi', userRoute);
 module.exports = app;
