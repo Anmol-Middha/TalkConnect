a09bfa8305485c1301ab0cce2c72e4fd124331cf
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-19 16:34:11 +0530

    update

--- api/Routes/user.js
+++ api/Routes/user.js
@@ -5,85 +5,31 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const config = require('../config/development');
 
-// Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
 passport.use(new GoogleStrategy({
     clientID: config.google.clientID,
     clientSecret: config.google.clientSecret,
     callbackURL: "https://talkconnect.herokuapp.com/google-auth/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      console.log(profile.emails[0].value);  
+      return done(err, user);
+    });
   }
 ));
 
+router.get('/', passport.authenticate('google', 
+{ scope: [
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email'
+]}
+));
 
-// const Scope = [
-//     'https://www.googleapis.com/auth/plus.me',
-//     'https://www.googleapis.com/auth/userinfo.email',
-// ];
-
-// const googleConfig = {
-//     cliendId: "760894743226-g62tkfeqdg19o05ssstl1629oodsrm7b.apps.googleusercontent.com",
-//     clientSecret: "mivbQO5VsXwIB-c47Gm20Nco",
-//     redirect: "https://localhost:8080/google-auth"
-// };
-
-// function createConnection(){
-//     return new google.auth.OAuth2(
-//         googleConfig.cliendId,
-//         googleConfig.clientSecret,
-//         googleConfig.redirect
-//     );
-// }
-// router.post('/', (req, res)=>{
-//     const OAuth = createConnection();
-//     const url = OAuth.generateAuthUrl({
-//         access_type: 'offline',
-//         propmt: 'consent',
-//         scope: Scope
-//     })
-//     console.log(url);
-//     res.send(url);
-// });
-
-// router.post('/signin', async(req, res)=>{
-//     const code = req.body.code;    
-//     const auth = createConnection();
-//     const data = await OAuth.getToken(code);
-//     const tokens = data.tokens;
-//     auth.setCredentials(tokens);
-//     const plus = getGooglePlusApi(auth);
-//     const me = await plus.people.get({ userId: 'me' });
-//     const userGoogleId = me.data.id;
-//     const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value;
-//     return {
-//       id: userGoogleId,
-//       email: userGoogleEmail,
-//       tokens: tokens,
-//     };
-// });
-
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve
-//   redirecting the user to google.com.  After authorization, Google
-//   will redirect the user back to this application at /auth/google/callback
-router.get('/', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 router.get('/callback', function(req, res) {
-  res.redirect('./home');
+  passport.authorize('google', {
+    successRedirect : '/dashboard',
+    failureRedirect : '/'
+  })
 });
 
-router.get('/home', (req, res)=>{
-    res.send("hello to home");
-});
 module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const userRoute = require('./api/Routes/user');
+const dashboardRoute = require('./api/Routes/dashboard');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
 
-app.get('/',(req, res)=>{
-    res.send("hello");
-})
-
 app.use('/google-auth', userRoute);
+app.use('/dashboard', dashboardRoute);
 
 module.exports = app;
