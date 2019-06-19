82a6530c6cc065f8e6e314c8e3a95f33977e9e7b
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-19 12:00:39 +0530

    update

--- api/Routes/user.js
+++ api/Routes/user.js
@@ -12,7 +12,7 @@ const config = require('../config/development');
 passport.use(new GoogleStrategy({
     clientID: config.google.clientID,
     clientSecret: config.google.clientSecret,
-    callbackURL: "https://localhost:8080/google-auth/callback"
+    callbackURL: "/google-auth/callback"
   },
   function(accessToken, refreshToken, profile, done) {
        User.findOrCreate({ googleId: profile.id }, function (err, user) {
