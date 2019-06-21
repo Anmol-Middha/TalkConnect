cabd350afae90a89851ed71594d8e1b0db51cc15
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-21 11:35:10 +0530

    update

--- api/config/auth.js
+++ api/config/auth.js
@@ -1,4 +1,4 @@
-const GoogleStrategy = require('passport-google-    oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const config = require('./development');
 module.exports = (passport) => {
     passport.serializeUser((user, done) => {
