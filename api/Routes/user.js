5b699c9685b5b13feea4d478314e352e7401609d
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-21 09:55:03 +0530

    update

--- api/Routes/user.js
+++ api/Routes/user.js
@@ -27,6 +27,7 @@ router.get('/', passport.authenticate('google',
 
 router.get('/callback', passport.authenticate('google', {failureRedirect:'/'}), function(req, res) {
   console.log(req.user.token);
+  res.send("hello");
 });
 
 module.exports = router;
