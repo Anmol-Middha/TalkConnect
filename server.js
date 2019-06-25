9d462f21dc8f193a3cb3dc8fe4f503d2d9fd03c3
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-25 16:17:46 +0530

    update

--- server.js
+++ server.js
@@ -15,12 +15,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
 
 app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Headers: Access-Control-Allow-Origin',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Access-Control-Allow-Headers ', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
-    if(req.method == 'OPTIONS'){
+    if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE');
         return res.status(200).json({});
     }
