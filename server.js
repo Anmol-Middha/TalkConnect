2de7d1a4bda9a6bcf4b252a8307cb961c6160eca
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-25 16:59:17 +0530

    update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use((req, res, next) =>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Headers ', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE');
