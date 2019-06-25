fadc35c8e530004532b2069d1b8bd2c486cf8a4c
Anmol Middha <anmolmiddha2827@gmail.com>
2019-06-25 16:10:05 +0530

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(morgan('dev'));
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
-        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Headers: Access-Control-Allow-Origin',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     if(req.method == 'OPTIONS'){
